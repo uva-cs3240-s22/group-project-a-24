15ef3c096ec1fdacbf70013ab696bf77868a490d
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2022-03-13 23:16:02 -0400

    added dummy model and test to check GithubCI

--- wordofmouth/models.py
+++ wordofmouth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
+
+class Temp(models.Model):
+    temp_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.temp_date
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.temp_date <= now
