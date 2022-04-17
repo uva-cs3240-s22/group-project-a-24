dea6c93a749d9dc810be54fd9bf3c8205fcf1134
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2022-04-17 19:03:15 -0400

    Update models.py

--- wordofmouth/models.py
+++ wordofmouth/models.py
@@ -32,6 +32,6 @@ class Recipe(models.Model):
     def __str__(self):
         return self.title
 
-class FavoriteRecipe(models.Model):
-    user = models.ForeignKey('User', related_name='favorites')
-    movie = models.ForeignKey('Movie', related_name='favorites')
+# class FavoriteRecipe(models.Model):
+#     user = models.ForeignKey('User', related_name='favorites')
+#     movie = models.ForeignKey('Movie', related_name='favorites')
