ab70415da404c66ff7ae3f6fb9ee0dbc54b8530c
sms4yv <64984826+sms4yv@users.noreply.github.com>
2022-04-28 13:22:04 -0400

    Form filled with prev values
    form now autopopulates on fork recipe

--- wordofmouth/views.py
+++ wordofmouth/views.py
@@ -33,7 +33,7 @@ def create_recipe(request):
 def fork_recipe(request, id):
     recipe = Recipe.objects.get(pk=id)
     if request.method == 'POST':
-        form = ForkedRecipeForm(request.GET, request.FILES, initial={"title": recipe.title, "description": recipe.description, "ingredients": recipe.ingredients, "directions": recipe.directions})
+        form = ForkedRecipeForm(request.POST, request.FILES)
         if form.is_valid():
             r = ForkedRecipe(title=form.cleaned_data['title'],
                        description=form.cleaned_data['description'],
@@ -46,7 +46,7 @@ def fork_recipe(request, id):
             r.save()
             return HttpResponseRedirect('/')
     else:
-        form = ForkedRecipeForm(request.GET, request.FILES, initial={"title": recipe.title, "description": recipe.description, "ingredients": recipe.ingredients, "directions": recipe.directions})
+        form = ForkedRecipeForm(instance=recipe)
     return render(request, 'templates/wordofmouth/fork-recipe.html', {'form': form, 'recipe': recipe})
 
 def view_recipes(request):
