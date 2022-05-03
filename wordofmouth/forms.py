2f7c3f2a24efe945392b4c9a5732956257089fd2
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2022-05-03 01:08:43 -0400

    updated overall styling of site

--- wordofmouth/forms.py
+++ wordofmouth/forms.py
@@ -4,8 +4,8 @@ from .models import Comment, Recipe
 class RecipeForm(forms.Form):
     title = forms.CharField(label='Enter Recipe Title', max_length=200)
     description = forms.CharField(label='Enter a Brief Description', max_length=200)
-    ingredients = forms.CharField(label='Enter Ingredients and Specified Amounts',widget=forms.Textarea(attrs={"rows":5, "cols":20}))
-    directions = forms.CharField(label='Enter Directions',widget=forms.Textarea(attrs={"rows":5, "cols":20}))
+    ingredients = forms.CharField(label='Enter Ingredients and Specified Amounts (please separate ingredients by clicking \'Enter\')',widget=forms.Textarea(attrs={"rows":5, "cols":20}))
+    directions = forms.CharField(label='Enter Directions (please separate directions by clicking \'Enter\')',widget=forms.Textarea(attrs={"rows":5, "cols":20}))
     image = forms.ImageField(label='Upload Image')
 
 class ForkedRecipeForm(forms.ModelForm):
